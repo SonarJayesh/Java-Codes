a7d4660cb61e386687d7e103fc7d0465ac43c023
SonarJayesh <jayeshsonar2824@gmail.com>
2024-02-23 19:30:49 +0530

    [ADDED]: JAYESH Java code for 2D array And Find Maximum no and Calculate Addtion

---
+++ Array2DAddMax.java
@@ -0,0 +1,44 @@
+
+import java.util.Scanner;
+class Array2DAddMax
+{
+	public static void main(String args[])
+	{
+		int a[]=new int[5];
+		int b[]=new int[5];
+		int sum[]=new int[5];
+		int i,max,max1;
+
+		System.out.println("Enter the First Array Element");
+		Scanner obj=new Scanner(System.in);
+
+		max=a[0];
+		for(i=0;i<a.length;i++)
+		{
+			a[i]=obj.nextInt();
+			if(max<a[i])
+			{
+				max=a[i];
+			}
+		}
+		System.out.println("Max="+max);
+
+		System.out.println("Enter the Second Array Element");
+		max1=b[0];
+		for(i=0;i<b.length;i++)
+		{
+			b[i]=obj.nextInt();
+			if(max1 < b[i])
+			{
+				max1=b[i];
+			}
+		}
+		System.out.println("Max1= "+max1);
+
+		for(i=0;i<sum.length;i++)
+		{
+			sum[i]=a[i]+b[i];
+			System.out.println("Sum= "+sum[i]);
+		}
+	}
+}
