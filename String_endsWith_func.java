c99c59ec651322453075944022de466e217f0be2
SonarJayesh <jayeshsonar2824@gmail.com>
2024-03-07 19:11:41 +0530

    [ADDED]: JAYESH Java code for find String Ends Witch character

---
+++ String_endsWith_func.java
@@ -0,0 +1,11 @@
+class String_endsWith_func
+{
+	public static void main(String args[]) {
+
+		String a = "Jayesh Sonar";
+
+		System.out.println("endsWith() = "+ a.endsWith("nar"));
+		System.out.println("endsWith() = "+ a.endsWith("aye"));
+		System.out.println("endsWith() = "+ a.endsWith("sh"));
+	}
+}
