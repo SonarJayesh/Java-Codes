73fea579f96faf9ac8e90f67ed78189a0d48597e
SonarJayesh <jayeshsonar2824@gmail.com>
2024-03-12 21:20:31 +0530

    [ADDED]: JAYESH Java code for String startWith()

---
+++ String_startWith_func.java
@@ -0,0 +1,11 @@
+class String_startWith_func
+{
+	public static void main(String args[]) {
+
+		String a = "Jayesh Sonar";
+
+		System.out.println("startsWith() = "+ a.startsWith("Jay"));
+		System.out.println("startsWith() = "+ a.startsWith("aye"));
+		System.out.println("startsWith() = "+ a.startsWith("sh"));
+	}
+}
