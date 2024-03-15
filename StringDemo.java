ab3b533299c7b80b42079fd95e69c4ab1fdafda7
SonarJayesh <jayeshsonar2824@gmail.com>
2024-03-15 20:57:50 +0530

    [ADDED]: JAYESH Java code for StringDemo

---
+++ StringDemo.java
@@ -0,0 +1,15 @@
+class StringDemo{
+
+	public static void main(String args[]){
+
+		String name1 ="Jayesh";
+		String name2 = new String("Darshan");
+		name2 ="Shubham";
+		System.out.println("Yogesh");
+
+
+		System.out.println(name1);
+		System.out.println(name2);
+		System.out.println(name2);
+		}
+	}
