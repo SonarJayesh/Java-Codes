f7250c1e1d6bb4e127435d5987b82aa34ba66794
SonarJayesh <jayeshsonar2824@gmail.com>
2024-03-19 19:01:13 +0530

    [ADDED]: JAYESH java code for String conCatNate Function

---
+++ StringconcatFunc.java
@@ -0,0 +1,11 @@
+class StringconcatFunc
+{
+	public static void main(String args[])
+	{
+		String a = "Jayesh";
+		String b = "Sonar";
+
+		System.out.println("Concatination = "+a.concat(b));
+
+	}
+}
