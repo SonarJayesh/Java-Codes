25b96cbf9674df553be2f7372f55b5bd4cd4db63
SonarJayesh <jayeshsonar2824@gmail.com>
2024-03-21 19:23:37 +0530

    [ADDED]: JAYESH Java code for String equal Function

---
+++ StringequalFunc.java
@@ -0,0 +1,13 @@
+class StringequalFunc
+{
+	public static void main(String args[]) {
+
+		String a = "Jayesh";
+		String b = "Jayesh";
+		String c = "Sonar";
+
+		System.out.println("String equals = "+a.equals(b));
+		System.out.println("String not equals = "+a.equals(c));
+
+	}
+}
