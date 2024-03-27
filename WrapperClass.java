7bd4e526eb3f5dd51184e92708bb2631e5f2819f
SonarJayesh <jayeshsonar2824@gmail.com>
2024-03-27 19:05:29 +0530

    [ADDED]: JAYESH Java code for Wrapper class

---
+++ WrapperClass.java
@@ -0,0 +1,12 @@
+class WrapperClass{
+
+	public static void main(String args[]){
+
+		int i = 100;
+		Integer iobj = Integer.valueOf(i);
+		Integer iobj1 = i;	// Autoboxing
+		System.out.println(i +" "+ iobj+ " "+iobj1);
+
+		}
+
+	}
