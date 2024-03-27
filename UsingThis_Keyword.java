4159a9e26c54255dce909dae88937d4179dce9ea
SonarJayesh <jayeshsonar2824@gmail.com>
2024-03-27 19:08:42 +0530

    [ADDED]: JAYESH Java code for Print valure for using this Keyword

---
+++ UsingThis_Keyword.java
@@ -0,0 +1,22 @@
+class UsingThis_Keyword{
+
+		int x;
+		UsingThis_Keyword(int x){
+				this.x = x;
+			}
+
+		public static void main(String[] args){
+
+			UsingThis_Keyword usingthis_keyword = new UsingThis_Keyword(10);
+			System.out.println("Value of X is: "+ usingthis_keyword.x);
+
+			}
+	}
+
+
+
+
+
+
+
+
