670c27e54b2618aeaa9f25f2112846857a07ec5a
SonarJayesh <jayeshsonar2824@gmail.com>
2024-04-08 19:32:15 +0530

    [ADDED]: JAYESH java code for Addition of two No Using command Line Argument

---
+++ CommandLine.java
@@ -0,0 +1,16 @@
+class CommandLine
+{
+	public static void main(String args[]){
+
+		int a , b , c = 0;
+
+		a = Integer.parseInt(args[0]);
+
+		b = Integer.parseInt(args[1]);
+
+		c = a + b;
+
+		System.out.println("Addition = "+ c);
+	}
+
+}
