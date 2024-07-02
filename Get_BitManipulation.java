b9868df13a11ebb3c61e64f7cf486b112edd32ad
SonarJayesh <jayeshsonar2824@gmail.com>
2024-07-02 22:17:22 +0530

    [ADDED]: JAYESH Java code for Get bit Maniplation program

---
+++ Get_BitManipulation.java
@@ -0,0 +1,15 @@
+public class Get_BitManipulation {
+
+     public static void main(String[] args) {
+        
+        int n = 5;
+        int pos = 2;
+        int bitMask = 1<<pos;
+
+        if((bitMask & n )== 0){
+            System.out.println("bit was zero");
+        }else{
+            System.out.println("bit was one");
+        }
+    }
+}
