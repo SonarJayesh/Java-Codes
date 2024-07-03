547daef7922bb65b28c8f348c6483b3e2e259cf6
SonarJayesh <jayeshsonar2824@gmail.com>
2024-07-03 21:08:32 +0530

    [ADDED]: JAYESH Java code for clear bit Manipulation technique in java

---
+++ Clear_BitManipulation.java
@@ -0,0 +1,11 @@
+public class Clear_BitManipulation {
+    public static void main(String[] args) {
+        int n = 5;
+        int pos = 2;
+        int bitMask = 1<<pos;
+        int notBitMask = ~(bitMask);
+
+        int newNumber = notBitMask & n;
+        System.out.println(newNumber);
+    }
+}
