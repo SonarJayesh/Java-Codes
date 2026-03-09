504a5e5f750708a24f78d5e4249b24b85fd8c979
SonarJayesh <jayeshsonar2824@gmail.com>
2026-03-10 00:04:39 +0530

    [ADDED]: Jayesh Java Interview Question for Reverse the Given String

---
+++ ReverseString.java
@@ -0,0 +1,14 @@
+public class ReverseString{
+
+	public static void main(String[] args){
+
+		String str = "Cognizant";
+		String reverse = "";
+		for(int i = str.length()-1; i>=0; i--){
+		
+			reverse += str.charAt(i);
+			
+		}
+		System.out.println(reverse);
+	}
+}
